e364b278baf5479b62a3f022fc9a601279daad42
Caleb <cjr214@gmail.com>
2021-05-04 21:57:45 -0400

    Commit project developed in repl.it

---
+++ mean_var_std.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+def calculate(list):
+  """
+    {
+  'mean': [axis1, axis2, flattened],
+  'variance': [axis1, axis2, flattened],
+  'standard deviation': [axis1, axis2, flattened],
+  'max': [axis1, axis2, flattened],
+  'min': [axis1, axis2, flattened],
+  'sum': [axis1, axis2, flattened]
+}
+"""
+  
+  if len(list) < 9:
+    raise ValueError("List must contain nine numbers.")
+
+  matrix = np.reshape(list, (3,3))
+  calculations = {
+    'mean': [np.mean(matrix, axis=0).tolist(), np.mean(matrix, axis=1).tolist(), np.mean(matrix).tolist()],
+    'variance': [np.var(matrix, axis=0).tolist(), np.var(matrix, axis=1).tolist(), np.var(matrix).tolist()],
+    'standard deviation': [np.std(matrix, axis=0).tolist(), np.std(matrix, axis=1).tolist(), np.std(matrix).tolist()],
+    'max': [np.max(matrix, axis=0).tolist(), np.max(matrix, axis=1).tolist(), np.max(matrix).tolist()],
+    'min': [np.min(matrix, axis=0).tolist(), np.min(matrix, axis=1).tolist(), np.min(matrix).tolist()],
+    'sum': [np.sum(matrix, axis=0).tolist(), np.sum(matrix, axis=1).tolist(), np.sum(matrix).tolist()],
+  }
+
+
+  return calculations
